0b726e82813c3db27208532cf966ca6040967f82
L0cket <74828682+L0cket@users.noreply.github.com>
2021-02-18 23:34:31 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,131 @@
+'use strict'
+
+function funcMenu(){
+    let menuBtn = document.getElementById('menu_btn');
+    let activeMenu = document.querySelector('.hidden_menu');
+    let burgerLines = document.querySelectorAll('.burger-line');
+    let burgerText = document.querySelector('.burger_text');
+
+    menuBtn.addEventListener('click', showMenu);
+
+    function showMenu(){
+        for(let i = 0; i < burgerLines.length; i++){
+            burgerLines[i].classList.toggle('active_lines');
+        }
+        burgerText.classList.toggle('active_burger_text');
+        activeMenu.classList.toggle('hidden_menu_active');
+        //document.querySelector('.header_nav').classList.toggle('hide_header_nav');
+    }
+}
+
+function showLanguage(){
+    const language = document.getElementById('localization');
+    let localLanguage = document.querySelectorAll('.language');
+    let arrow = document.querySelectorAll('.language_arrow');
+    const localMenu = document.getElementById('local_menu');
+    const langType = document.querySelector('.language_type');
+    let localLangType = document.querySelectorAll('.language_language');
+
+    let index = 0;
+
+    language.addEventListener('click', showLocalMenu);
+
+    localLanguage.forEach(function(item, indexLanguage){
+        item.addEventListener('click', function(){
+            index = indexLanguage;
+            activeLanguage(index);
+            activeArrow(index);
+            activeLanguageType(index);
+        })
+    });
+
+    function showLocalMenu(){
+        localMenu.classList.toggle('active_localization_menu');
+        langType.classList.toggle('localization_active');
+    }
+
+    function activeLanguage(n){
+        for(let i = 0; i < localLanguage.length; i++){
+            localLanguage[i].classList.remove('language_active');
+        }
+        localLanguage[n].classList.add('language_active');
+    }
+
+    function activeArrow(n){
+        for(let i = 0; i < arrow.length; i++){
+            arrow[i].classList.remove('active_arrow');
+        }
+        arrow[n].classList.add('active_arrow');
+    }
+    
+    function activeLanguageType(n){
+        langType.innerHTML = localLangType[n].getAttribute('key');
+    }
+}
+
+function funcSlider(){
+    let dots = document.querySelectorAll('.slider_circle');
+    let sliderNumber = document.getElementById('slider_number');
+    let index = 0;
+
+    function activeDots(n){
+        for(let i = 0; i < dots.length; i++){
+            dots[i].classList.remove('slider_active');
+        }
+        dots[n].classList.add('slider_active');
+    }
+
+    function activeSlider(n){
+        if(n == 0){
+            sliderNumber.innerText = '01';
+        } else{
+            sliderNumber.innerText = '0' + (n + 1);
+        }
+    }
+
+    dots.forEach(function(item, indexDot){
+        item.addEventListener('click', function(){
+            index = indexDot;
+            activeSlider(index);
+            activeDots(index);
+        })
+    });
+}
+
+function moveFunc(){
+    const slider = document.getElementById('slider');
+    const text = document.getElementById('headerDescription');
+    const content = document.querySelector('.header_content');
+    const headerCenter = document.querySelector('.header_center');
+
+    const headerNav = document.querySelector('.header_nav');
+    const menuHidden = document.querySelector('.hidden_menu');
+    const headerMenu = document.querySelector('.menu_navbar');
+
+    const support = document.querySelector('.header_support');
+    const supportMenu = document.querySelector('.header_menu');
+
+    window.addEventListener('resize', moveBlocks);
+    window.addEventListener('load', moveBlocks);
+
+    function moveBlocks(){
+        if(window.innerWidth <= 835){
+            headerCenter.append(text, slider);
+            menuHidden.prepend(headerNav);
+        } else{
+            content.append(slider, text);
+            headerMenu.append(headerNav);
+            supportMenu.append(support);
+        }
+
+        if(window.innerWidth <= 500){
+            menuHidden.insertBefore(support, menuHidden.children[1]);
+        } else{
+            supportMenu.append(support);
+        }
+    }
+}
+funcMenu();
+showLanguage();
+funcSlider();
+moveFunc();
